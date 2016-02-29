bf1f4110b13a3150e42e7e098dfb5524ff1a8c55
Abel <sdwyub@qq.com>
2016-02-29 17:11:13 +0800

    add api.js ;

--- js/controller/index.js
+++ js/controller/index.js
@@ -1,5 +1,30 @@
 ;(function(){
-  app.controller('index',function($scope,$http){
-    
+  app.controller('index',function($scope,$http,API){
+    API.getAvailableTime(function(data){
+      $scope.times = data.data;
+    });
+
+    $scope.today=new Date().getTime();
+
+    /**
+     * 选择
+     */
+    $scope.chooseTime=function(time){
+      $scope.choose=time;
+    }
+
+    /**
+     * 预订
+     */
+    $scope.book=function(time){
+      API.book({
+        userId:"10555",
+        weixinId:"10555",
+        phone:"123456789101",
+        signUpId:time.id
+      },function(data){
+        console.log(data)
+      })
+    }
   });
 })();
